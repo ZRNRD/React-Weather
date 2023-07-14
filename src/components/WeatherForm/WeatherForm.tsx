36eb127da2bacf2a8f461826f9b2767f19d45a52
Владимир Филимоненко <zrnrd@mail.ru>
2023-07-14 21:15:07 +0500

    Fixed WeatherForm styles

--- src/components/WeatherForm/WeatherForm.tsx
+++ src/components/WeatherForm/WeatherForm.tsx
@@ -8,31 +8,31 @@ type T_WeatherForm = PropsWithChildren<{ city?: string;}>;
 
 export const WeatherForm: FC<T_WeatherForm> = ({ children, city }) => {
   return (
-    <div className="container">
-        <div className="weather">
-            <div className="weather__show">
-                <h1 className="city-name"></h1>
-                <p className="current-time"></p>
-                <div className="current-weather">
-                    <div className="current-weather__top">
-                        <p className="temperature"></p>
+    <div className={style["container"]}>
+        <div className={style["weather"]}>
+            <div className={style["weather__show"]}>
+                <h1 className={style["city-name"]}>Город</h1>
+                <p className={style["current-time"]}>21:00</p>
+                <div className={style["current-weather"]}>
+                    <div className={style["current-weather__top"]}>
+                        <p className={style["temperature"]}>18°C</p>
                         
-                        <p className="description"></p>
+                        <p className={style["description"]}> Гроза</p>
                     </div>
-                    <div className="current-weather__bottom">
-                        <p className="wind"></p>
-                        <p className="humidity"></p>
-                        <p className="pressure"></p>
+                    <div className={style["current-weather__bottom"]}>
+                        <p className={style["wind"]}>Ветер 4 м/с</p>
+                        <p className={style["humidity"]}>Влажность 100%</p>
+                        <p className={style["pressure"]}>Давление: 746 мм рт. ст.</p>
                     </div>
                 </div>
             </div>
 
-            <div className="weather__get">
+            <div className={style["weather__get"]}>
                 <h2>Введите город</h2>
-                <input type="text" className="city-input" placeholder="Название города" />
-                <div className="cities__list"></div>
+                <input type="text" className={style["city-input"]} placeholder="Название города" />
+                <div className={style["cities__list"]}></div>
                 
-                <button className="get-weather">Запросить погоду</button>
+                <button className={style["get-weather"]}>Запросить погоду</button>
             </div>
             
         </div>
